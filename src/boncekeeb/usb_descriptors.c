3053253ec5e24cb3d29e896abe0289ce27c8f7ff
Bonce <chris@headstrong.solutions>
2021-08-15 00:16:14 +0100

    Aligned with official TinyUSB report

--- src/boncekeeb/usb_descriptors.c
+++ src/boncekeeb/usb_descriptors.c
@@ -1,4 +1,4 @@
-/*
+/* 
  * The MIT License (MIT)
  *
  * Copyright (c) 2019 Ha Thach (tinyusb.org)
@@ -41,23 +41,23 @@
 //--------------------------------------------------------------------+
 tusb_desc_device_t const desc_device =
 {
-	.bLength            = sizeof(tusb_desc_device_t),
-	.bDescriptorType    = TUSB_DESC_DEVICE,
-	.bcdUSB             = 0x0200,
-	.bDeviceClass       = 0x00,
-	.bDeviceSubClass    = 0x00,
-	.bDeviceProtocol    = 0x00,
-	.bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
+    .bLength            = sizeof(tusb_desc_device_t),
+    .bDescriptorType    = TUSB_DESC_DEVICE,
+    .bcdUSB             = 0x0200,
+    .bDeviceClass       = 0x00,
+    .bDeviceSubClass    = 0x00,
+    .bDeviceProtocol    = 0x00,
+    .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
 
-	.idVendor           = 0xBC05,
-	.idProduct          = USB_PID,
-	.bcdDevice          = 0x0100,
+    .idVendor           = 0x0B01,
+    .idProduct          = USB_PID,
+    .bcdDevice          = 0x0001,
 
-	.iManufacturer      = 0x01,
-	.iProduct           = 0x02,
-	.iSerialNumber      = 0x03,
+    .iManufacturer      = 0x01,
+    .iProduct           = 0x02,
+    .iSerialNumber      = 0x03,
 
-	.bNumConfigurations = 0x01
+    .bNumConfigurations = 0x01
 };
 
 // Invoked when received GET DEVICE DESCRIPTOR
@@ -73,45 +73,10 @@ uint8_t const * tud_descriptor_device_cb(void)
 
 uint8_t const desc_hid_report[] =
 {
-    HID_USAGE_PAGE ( HID_USAGE_PAGE_DESKTOP     )                    ,\
-    HID_USAGE      ( HID_USAGE_DESKTOP_KEYBOARD )                    ,\
-    HID_COLLECTION ( HID_COLLECTION_APPLICATION )                    ,\
-    /* Report ID if any */\
-    HID_REPORT_ID(REPORT_ID_KEYBOARD) \
-    /* 8 bits Modifier Keys (Shfit, Control, Alt) */ \
-    HID_USAGE_PAGE ( HID_USAGE_PAGE_KEYBOARD )                     ,\
-    HID_USAGE_MIN    ( 224                                    )  ,\
-    HID_USAGE_MAX    ( 231                                    )  ,\
-    HID_LOGICAL_MIN  ( 0                                      )  ,\
-    HID_LOGICAL_MAX  ( 1                                      )  ,\
-    HID_REPORT_COUNT ( 8                                      )  ,\
-    HID_REPORT_SIZE  ( 1                                      )  ,\
-    HID_INPUT        ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE )  ,\
-    /* 8 bit reserved */ \
-    HID_REPORT_COUNT ( 1                                      )  ,\
-    HID_REPORT_SIZE  ( 8                                      )  ,\
-    HID_INPUT        ( HID_CONSTANT                           )  ,\
-    /* 6-byte Keycodes */ \
-    HID_USAGE_PAGE ( HID_USAGE_PAGE_KEYBOARD )                     ,\
-    HID_USAGE_MIN    ( 0                                   )     ,\
-    HID_USAGE_MAX    ( 255                                 )     ,\
-    HID_LOGICAL_MIN  ( 0                                   )     ,\
-    HID_LOGICAL_MAX  ( 255                                 )     ,\
-    HID_REPORT_COUNT ( 6                                   )     ,\
-    HID_REPORT_SIZE  ( 8                                   )     ,\
-    HID_INPUT        ( HID_DATA | HID_ARRAY | HID_ABSOLUTE )     ,\
-    /* 5-bit LED Indicator Kana | Compose | ScrollLock | CapsLock | NumLock */ \
-    HID_USAGE_PAGE  ( HID_USAGE_PAGE_LED                   )       ,\
-    HID_USAGE_MIN    ( 1                                       ) ,\
-    HID_USAGE_MAX    ( 5                                       ) ,\
-    HID_REPORT_COUNT ( 5                                       ) ,\
-    HID_REPORT_SIZE  ( 1                                       ) ,\
-    HID_OUTPUT       ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE  ) ,\
-    /* led padding */ \
-    HID_REPORT_COUNT ( 1                                       ) ,\
-    HID_REPORT_SIZE  ( 3                                       ) ,\
-    HID_OUTPUT       ( HID_CONSTANT                            ) ,\
-    HID_COLLECTION_END \
+  TUD_HID_REPORT_DESC_KEYBOARD( HID_REPORT_ID(REPORT_ID_KEYBOARD         )),
+  TUD_HID_REPORT_DESC_MOUSE   ( HID_REPORT_ID(REPORT_ID_MOUSE            )),
+  TUD_HID_REPORT_DESC_CONSUMER( HID_REPORT_ID(REPORT_ID_CONSUMER_CONTROL )),
+  TUD_HID_REPORT_DESC_GAMEPAD ( HID_REPORT_ID(REPORT_ID_GAMEPAD          ))
 };
 
 // Invoked when received GET HID REPORT DESCRIPTOR
@@ -142,8 +107,8 @@ uint8_t const desc_configuration[] =
   // Config number, interface count, string index, total length, attribute, power in mA
   TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
 
-  // Interface number, string index, protocol, report descriptor len, EP In & Out address, size & polling interval
-  TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_HID, CFG_TUD_HID_BUFSIZE, 10)
+  // Interface number, string index, protocol, report descriptor len, EP In address, size & polling interval
+  TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 5)
 };
 
 // Invoked when received GET CONFIGURATION DESCRIPTOR
@@ -160,19 +125,19 @@ uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
 //--------------------------------------------------------------------+
 
 // array of pointer to string descriptors
-char const *string_desc_arr[] =
+char const* string_desc_arr [] =
 {
-  (const char[]) {0x09, 0x04},    // 0: is supported language is English (0x0409)
-  "Headstrong Solutions",         // 1: Manufacturer
-  "Bonce_Keeb",                   // 2: Product
-  "0001",                         // 3: Serials, should use chip ID
+  (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
+  "Headstronf Solutions",                     // 1: Manufacturer
+  "Bonce Keeb",              // 2: Product
+  "hs00000001",                      // 3: Serials, should use chip ID
 };
 
 static uint16_t _desc_str[32];
 
 // Invoked when received GET STRING DESCRIPTOR request
 // Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
-uint16_t const * tud_descriptor_string_cb(uint8_t index, uint16_t langid)
+uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
 {
   (void) langid;
 
@@ -182,18 +147,18 @@ uint16_t const * tud_descriptor_string_cb(uint8_t index, uint16_t langid)
   {
     memcpy(&_desc_str[1], string_desc_arr[0], 2);
     chr_count = 1;
-    }
-  else
+  }else
   {
-    // Convert ASCII string into UTF-16
+    // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
+    // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
 
-    if ( !(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0]))) return NULL;
+    if ( !(index < sizeof(string_desc_arr)/sizeof(string_desc_arr[0])) ) return NULL;
 
     const char* str = string_desc_arr[index];
 
     // Cap at max char
     chr_count = strlen(str);
-    if ( chr_count > 31) chr_count = 31;
+    if ( chr_count > 31 ) chr_count = 31;
 
     // Convert ASCII string into UTF-16
     for(uint8_t i=0; i<chr_count; i++)
